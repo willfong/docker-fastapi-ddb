c6ad6541daf7806c5a9b366a3bd36597633c663a
Will Fong <willfong@pm.me>
2020-01-31 22:39:21 +0800

    remove old scores, updated logging

--- app/services/util.py
+++ app/services/util.py
@@ -3,6 +3,10 @@ import requests
 import logging
 
 logger = logging.getLogger(__name__)
+myFormatter = logging.Formatter('[%(asctime)s] [%(levelname)s] %(message)s')
+handler = logging.StreamHandler()
+handler.setFormatter(myFormatter)
+logger.addHandler(handler)
 
 def get_secret_token():
     # TODO: Obviously, this should be updated to AWS KMS
