20979bfa1221abacfd80e220ba037ed895b06192
Will Fong <willfong@pm.me>
2020-03-18 08:08:54 +0800

    Add font awesome

--- vue/src/main.js
+++ vue/src/main.js
@@ -4,6 +4,15 @@ import router from './router'
 import store from './store'
 import 'bulma/css/bulma.css'
 import GSignInButton from 'vue-google-signin-button'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+// See: https://github.com/FortAwesome/vue-fontawesome
+library.add(faUserSecret)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
 
 Vue.config.productionTip = false
 Vue.config.devtools = true
